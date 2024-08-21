e3dc5eafb2da92dec8ea6b178bc52864c225a8b9
Francisco <f.lizama05@ufromail.cl>
2024-08-21 10:28:02 -0400

    Metodo para obtener cartas y eliminar cartas de la baraja listo y una base del menu principal

--- src/main/java/JuegoDeCartas.java
+++ src/main/java/JuegoDeCartas.java
@@ -1,14 +1,52 @@
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class JuegoDeCartas {
     public static void main(String[] args) {
 
     }
+    //Metodos del menu
+    public static void menuPrincipal(){
+
+        System.out.println("------------ MenuPrincipal ------------");
+        System.out.println("1. Jugar");
+        System.out.println("2. Salir");
+        accionesMenu();
+    }
+
+    public static void accionesMenu() {
+        int seleccionUsuario = seleccionUsuario();
+        switch (seleccionUsuario) {
+            case 1:
 
 
+        }
+
+    }
+    //Este metodo comprueba la seleccion del usuario y que cumpla los requisitos necesarios
+    public static int seleccionUsuario(){
+        int numeroOpciones = 2; //Valor modificable segun las opciones establecidas
+        while (true) {
+            int seleccionUsuario=0;
+            try {
+                seleccionUsuario = obtenerEntero();
+                if (seleccionUsuario>0 && seleccionUsuario<=numeroOpciones){
+                    return seleccionUsuario;
+                }else {
+                    throw new InputMismatchException();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Opcion Invalida!");
+            }
+        }
+    }
 
 
-
+    public static int obtenerEntero(){
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextInt();
+    }
 
     //El metodo devuelve una matriz con las cartas de un jugador
     public static String[][] obtenerCartas(String[][] matrizBarajaCartas) {
